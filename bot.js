21eabb9e9129be65735a3d579fb50fb0d67d4615
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2019-01-14 20:03:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -67,6 +67,32 @@ client.on('guildMemberAdd', member => {
 });
 
 
+exports.run = function(client, message, args) {
+
+  if (!message.guild) {
+    return message.author.send('`temizle` **komutu sadece sunucularda kullanılabilir.**');
+  }
+  let mesajsayisi = parseInt(args.join(' '));
+  if (mesajsayisi.length < 1) return message.channel.send('**Kaç mesaj silmem gerektiğini belirtmedin.**')
+  if (mesajsayisi > 100) return message.channel.send('**__100__** **adetden fazla mesaj silemem!**');
+  message.channel.bulkDelete(mesajsayisi + 1);
+  message.channel.send('**__' + mesajsayisi + '__** **adet mesaj sildim!** ')
+};
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: ['sil'],
+  permLevel: 2
+};
+
+exports.help = {
+  name: 'temizle',
+  description: 'Belirlenen miktar mesajı siler.',
+  usage: 'temizle <temizlenecek mesaj sayısı>'
+};
+
+
 
 
 client.login(process.env.BOT_TOKEN);
