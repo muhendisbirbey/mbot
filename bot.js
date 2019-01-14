1beb9afeb12a2c201a43612372704f3457e4667f
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2019-01-14 21:59:48 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,23 +1,14 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment');
-const prefix = '!';
 
-
-
-client.on('ready', () => {
-  client.user.setGame('Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :) ', 'https://www.twitch.tv/muhendisbeymuhendishanim')
-});
-
-
-
-
-
-		
-		
+client.on('ready', () => 
+	  {client.user.setGame('Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :) ', 'https://www.twitch.tv/muhendisbeymuhendishanim')});
+	client.on('message', msg => {	
   if (msg.content === 'sa') {
     msg.reply('Aleyküm Selam hoşgeldin');
   }
+
 });
 
 client.on('message', msg => {
@@ -73,11 +64,5 @@ client.on('guildMemberAdd', member => {
 client.on('guildMemberAdd', member => {
    member.send(' Discord sunucumuza hoşgeldin. Sunucudaki diğer odaları görebilmek için #rol-secimi kanalından rollerini alabilirsin.  Ayrıca #kurallar-kanaladavet kanalından sunucu kurallarını okumayı da unutma :slight_smile:  İyi eğlenceler :heart: ');
 });
-
-
-
-
-
-
 
 client.login(process.env.BOT_TOKEN);
