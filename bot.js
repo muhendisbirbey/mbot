a90e974c0598db8a93bc68b56a644268f981139f
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2019-01-14 21:12:35 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,68 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const moment = require('moment');
-
-
 const prefix = '!';
+
+
 
 client.on('ready', () => {
   client.user.setGame('Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :) ', 'https://www.twitch.tv/muhendisbeymuhendishanim')
 });
 
+
+bot.on('message', message => {
+
+    // Variables - Variables make it easy to call things, since it requires less typing.
+    let msg = message.content.toUpperCase(); // This variable takes the message, and turns it all into uppercase so it isn't case sensitive.
+    let sender = message.author; // This variable takes the message, and finds who the author is.
+    let cont = message.content.slice(prefix.length).split(" "); // This variable slices off the prefix, then puts the rest in an array based off the spaces
+    let args = cont.slice(1); // This slices off the command in cont, only leaving the arguments.
+
+    // Commands
+
+    // Ping
+    if (msg === prefix + 'ping') { // This checks if msg (the message but in all caps), is the same as the prefix + the command in all caps.
+
+        // Now, let's send a response.
+        message.channel.send('Ping!'); // This 'sends' the message to the channel the message was in. You can change what is in the message to whatever you want.
+
+    }
+
+
+    // Purge
+    if (msg.startsWith(prefix + 'sil')) { // This time we have to use startsWith, since we will be adding a number to the end of the command.
+        // We have to wrap this in an async since awaits only work in them.
+        async function purge() {
+            message.delete(); // Let's delete the command message, so it doesn't interfere with the messages we are going to delete.
+
+            // Now, we want to check if the user has the `bot-commander` role, you can change this to whatever you want.
+            if (!message.member.roles.find("name", "MÜHENDİSLER")) { // This checks to see if they DONT have it, the "!" inverts the true/false
+                message.channel.send('Bu komutu kullanabilmek için yetkiniz yok!'); // This tells the user in chat that they need the role.
+                return; // this returns the code, so the rest doesn't run.
+            }
+
+            // We want to check if the argument is a number
+            if (isNaN(args[0])) {
+                // Sends a message to the channel.
+                message.channel.send('Lütfen numara kullanın. \n Kullanım şekli : ' + prefix + 'sil <değer>'); //\n means new line.
+                // Cancels out of the script, so the rest doesn't run.
+                return;
+            }
+
+            const fetched = await message.channel.fetchMessages({limit: args[0]}); // This grabs the last number(args) of messages in the channel.
+            console.log(fetched.size + ' mesaj bulundu, siliniyor...'); // Lets post into console how many messages we are deleting
+
+            // Deleting the messages
+            message.channel.bulkDelete(fetched)
+                .catch(error => message.channel.send(`Error: ${error}`)); // If it finds an error, it posts it into the channel.
+
+        }
+
+        // We want to make sure we call the function whenever the purge command is run.
+        purge(); // Make sure this is inside the if(msg.startsWith)
+
+    }
+});
 
 
 
