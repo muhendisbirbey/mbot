843c9f35be0214b6c26a4f773567c38c44879fcd
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2019-01-17 10:53:44 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ bot.on('ready', () =>
 	  {bot.user.setGame('Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :) ', 'https://www.twitch.tv/muhendisbeymuhendishanim')});
 	
 bot.on('message', msg => {	
-  if (msg.content === 'sa' | msg.content ==='Sa'|msg.content ==='selamlar'|msg.content ==='sea'|msg.content ==='Sea' | msg.content ==='selam' | msg.content ==='Selamlar' | msg.content ==='Selam'){
+  if (msg.content === 's.a.' |msg.content === 's.a' |msg.content === 's' |msg.content === 'selm' |msg.content === 'salam' |msg.content === 'slm' |msg.content === 'sa' | msg.content ==='Sa'|msg.content ==='selamlar'|msg.content ==='sea'|msg.content ==='Sea' | msg.content ==='selam' | msg.content ==='Selamlar' | msg.content ==='Selam'){
     msg.reply('Aleyküm Selam Hoş Geldin');
   }
 
