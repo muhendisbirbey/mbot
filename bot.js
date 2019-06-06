e265002081370c5a0743803b51cab0700c1e177a
muhendisbirbey <42184009+muhendisbirbey@users.noreply.github.com>
2019-06-06 19:32:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,11 +1,30 @@
 const Discord = require('discord.js');
+const chalk = require('chalk');
 const bot = new Discord.Client();
 const moment = require('moment');
 const prefix = '!';
 
-bot.on('ready', () => 
+/*bot.on('ready', () => 
 	 {bot.user.setGame('Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :) ', 'https://www.twitch.tv/muhendisbeymuhendishanim')});
-   
+   */
+
+module.exports = client => {
+var oyun = [
+        "YouTube > Mühendis Bey ile Mühendis Hanım ",
+        "Instagram > @muhendisbeyilemuhendishanim ",
+	"DLive.tv/muhendisbeyilhanim ",
+	"Mühendis BOT  " +client.guilds.size+ "  sunucuda  " + client.users.size +" kullanıcıya hizmet veriyor!",
+	"Çalışmalar devam ediyor. Twitch kanalımıza gitmek için İZLE butonuna basabilirsin :)"
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setGame(oyun[random], "https://www.twitch.tv/muhendisbeymuhendishanim" );
+        }, 2 * 2500);
+	
+	
 bot.on('message', msg => {	
   if (msg.content === 's.a.' |msg.content === 's.a' |msg.content === 's' |msg.content === 'selm' |msg.content === 'salam' |msg.content === 'slm' |msg.content === 'sa' | msg.content ==='Sa'|msg.content ==='selamlar'|msg.content ==='sea'|msg.content ==='Sea' | msg.content ==='selam' | msg.content ==='Selamlar' | msg.content ==='Selam'){
     msg.reply('Aleyküm Selam Hoş Geldin');
